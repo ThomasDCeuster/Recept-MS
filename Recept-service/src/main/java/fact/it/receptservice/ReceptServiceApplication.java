e36fcd26eef236b43ebaa78751c70263288b46c3
Thomas De Ceuster <r0882755@student.thomasmore.be>
2023-10-11 09:01:34 +0200

    Initial commit

---
+++ Recept-service/src/main/java/fact/it/receptservice/ReceptServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.receptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptServiceApplication.class, args);
+    }
+
+}
---
+++ Recept-service/src/test/java/fact/it/receptservice/ReceptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.receptservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
