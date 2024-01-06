acf03543e4635beb75b5f86bbd622b1773c8baae
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-06 23:44:42 +0100

    sdfgdfgsdf

--- user-service/src/main/java/fact/it/userservice/repository/UserRepository.java
+++ user-service/src/main/java/fact/it/userservice/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package fact.it.userservice.repository;
 
 import fact.it.userservice.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByIdIn(String id);
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByIdIn(Long id);
 }
