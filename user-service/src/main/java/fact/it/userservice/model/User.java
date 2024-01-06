abeebe1fd3c3c850261e52546acf3a6007cfd2a7
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-06 21:14:14 +0100

    qsdqsdq

--- user-service/src/main/java/fact/it/userservice/model/User.java
+++ user-service/src/main/java/fact/it/userservice/model/User.java
@@ -6,14 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-
 import java.util.List;
-
 
 @Entity
 @Table(name = "users")
