176d60bff7861be0a4b6f7f4d6e28d1ffd97d5b0
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-06 21:20:48 +0100

    ghjghjh

--- user-service/src/main/java/fact/it/userservice/model/User.java
+++ user-service/src/main/java/fact/it/userservice/model/User.java
@@ -1,22 +1,21 @@
-package fact.it.userservice.model;
+package fact.it.ratingservice.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String username;
+    private String name;
 }
