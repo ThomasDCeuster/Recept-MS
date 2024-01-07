dfc45cbe6f95a5235ee555279d08257e43f3c50c
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-07 12:45:33 +0100

    Update RatingRepository.java

--- rating-service/src/main/java/fact/it/ratingservice/repository/RatingRepository.java
+++ rating-service/src/main/java/fact/it/ratingservice/repository/RatingRepository.java
@@ -3,10 +3,11 @@ package fact.it.ratingservice.repository;
 import fact.it.ratingservice.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating, String> {
-    List<Rating> findByNameIn(List<String> names);
+    List<Rating> findByNameIn(Collection<String> name);
 }
 
 
