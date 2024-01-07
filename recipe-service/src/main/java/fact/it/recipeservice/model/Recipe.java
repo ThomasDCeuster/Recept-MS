bce300912f07420b4171376d3bf977daf7f0a058
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-07 13:35:34 +0100

    loaddata recipe

--- recipe-service/src/main/java/fact/it/recipeservice/model/Recipe.java
+++ recipe-service/src/main/java/fact/it/recipeservice/model/Recipe.java
@@ -1,11 +1,13 @@
 package fact.it.recipeservice.model;
 
+import fact.it.ingredientservice.model.Ingredient;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -20,6 +22,7 @@ public class Recipe {
     private Long id;
     private String recipeNumber;
     private String name;
+
     @OneToMany(cascade = CascadeType.ALL)
     private List<RecipeLineItem> recipeLineItemsList;
 }
