01d505769d20163afe8b3826e1a8f63a846dba97
Thomas De Ceuster <r0882755@student.thomasmore.be>
2024-01-07 13:38:45 +0100

    Update Recipe.java

--- recipe-service/src/main/java/fact/it/recipeservice/model/Recipe.java
+++ recipe-service/src/main/java/fact/it/recipeservice/model/Recipe.java
@@ -1,13 +1,11 @@
 package fact.it.recipeservice.model;
 
-import fact.it.ingredientservice.model.Ingredient;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
