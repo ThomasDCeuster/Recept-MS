9b0eea0047da0d2d4ed58eee063356d79bca2d54
Jorrit Leysen <r0878912@student.thomasmore.be>
2024-01-07 17:28:18 +0100

    exception

--- recipe-service/src/main/java/fact/it/recipeservice/service/RecipeService.java
+++ recipe-service/src/main/java/fact/it/recipeservice/service/RecipeService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import fact.it.recipeservice.exception.RecipeNotFoundException;
 
 import java.util.Arrays;
 import java.util.List;
